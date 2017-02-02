7b6e21209e660a1ee6abe7e769dae480c859bf3f
Belal Taher <bt72@duke.edu>
2017-02-01 22:40:23 -0500

    Functioning integration of grid class

--- src/cellsociety_team09/model/Grid.java
+++ src/cellsociety_team09/model/Grid.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
 
+import cellsociety_team09.configuration.XMLReader;
+
 public class Grid {	
 
 	Cell[][] myGrid;
