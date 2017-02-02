6fadf9bb8c7ff2875dd627c446723e4ba43c884c
Tahia Emran <tahia.emran@duke.edu>
2017-02-01 22:56:43 -0500

    made changes to display

--- src/cellsociety_team09/view/Display.java
+++ src/cellsociety_team09/view/Display.java
@@ -1,5 +1,27 @@
 package cellsociety_team09.view;
 
-public class Display {
+import javafx.scene.Group;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
+public class Display {
+	Group root; 
+	private final int displayX = 600; 
+	private final int displayY = 800; 
+	private final int controlY = 200; 
+	public final int gridY = 600; 
+	public Display(){
+		root = new Group();
+	}
+	public Group getRoot(){
+		return root; 
+	}
+	// create control HBOX
+	private HBox createControls(){
+		HBox controlPane = new HBox(); 
+		controlPane.setPrefWidth(displayX);
+		controlPane.setPrefHeight(controlY);
+		return controlPane;
+	}
+	// create Grid part + Animate 
 }
