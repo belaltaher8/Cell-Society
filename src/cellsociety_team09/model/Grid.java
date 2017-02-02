19ca1bada1e94e52be8bd793fc50a0bba7f1e458
Belal Taher <bt72@duke.edu>
2017-02-01 23:06:04 -0500

    commented grid class

--- src/cellsociety_team09/model/Grid.java
+++ src/cellsociety_team09/model/Grid.java
@@ -14,11 +14,12 @@ public class Grid {
 	Rule myRule;
 
 	public Grid(XMLReader myReader){
+		//uses XML reader to store important information
 		amountOfRows = myReader.getHeight();
 		amountOfCols = myReader.getWidth(); 
 		myRule = myReader.getRule();
 		Integer[][] initialStates = myReader.getInitialGrid();
-		
+		//creates cells based on info from XMLreader
 		for(int col = 0; col < amountOfCols; col++){
 			for(int row = 0; row < amountOfRows; row++){
 				Integer initialState = initialStates[col][row];
@@ -30,13 +31,17 @@ public class Grid {
 			
 	
 	public Cell getCellAtPoint(Point myPoint){
+		//finds x and y of given point and locates the cell in the grid 
+		//at that point
 		int myX = myPoint.getX();
 		int myY = myPoint.getY();
 		return myGrid[myY][myX];
 	}
 	
 	public void stepGrid(){
+		//computes next states of cells
 		computeNextGrid();
+		//advances cells to their next state
 		advanceGrid();
 		
 	}
@@ -54,20 +59,20 @@ public class Grid {
 				List<Cell> myNeighbors = new ArrayList<Cell>();
 				//creates list to store all neighbor states
 				List<Integer> myNeighborStates = new ArrayList<Integer>();
-				
+				//Stores points of all neighbors
 				for(int k = 0 ; k < myCellNeighborsCoords.size(); k++){
 					myPoints.add(myCellNeighborsCoords.get(k));
 				}
-				
+				//locates all neighbors by stored points
 				for(int k = 0; k < myPoints.size(); k++){
 					Point currentPoint = myPoints.get(k);
 					myNeighbors.add(getCellAtPoint(currentPoint));
 				}
-				
+				//get states of all neighbors
 				for(int k = 0; k < myNeighbors.size(); k++){
 					myNeighborStates.add(myNeighbors.get(k).getState());
 				}
-				
+				//computes next state of current cell by states of neighbors
 				myCell.computeNextState(myNeighborStates);
 			}
 		}
@@ -76,6 +81,7 @@ public class Grid {
 	public void advanceGrid(){
 		for(int currentCol = 0; currentCol < amountOfCols; currentCol++){
 			for(int currentRow = 0; currentRow < amountOfRows; currentRow++){
+				//advances state of each individual cell
 				myGrid[currentCol][currentRow].advanceState();
 			}
 		}
