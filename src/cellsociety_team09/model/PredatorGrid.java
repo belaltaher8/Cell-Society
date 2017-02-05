59493e438ff883231f408088a2dffb009b0c32ed
Jay Doherty <jld60@duke.edu>
2017-02-05 18:43:45 -0500

    Made PredatorGrid class.

---
+++ src/cellsociety_team09/model/PredatorGrid.java
@@ -0,0 +1,12 @@
+package cellsociety_team09.model;
+
+import cellsociety_team09.configuration.XMLReader;
+
+public class PredatorGrid extends Grid {
+
+	public PredatorGrid(XMLReader reader) {
+		super(reader);
+		// TODO Auto-generated constructor stub
+	}
+
+}
