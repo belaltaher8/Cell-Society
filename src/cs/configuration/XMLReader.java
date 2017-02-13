daff5443a7fad9a4a07f9dad611316e7f686443d
Jay Doherty <jld60@duke.edu>
2017-02-12 22:01:25 -0500

    More comments

--- src/cs/configuration/XMLReader.java
+++ src/cs/configuration/XMLReader.java
@@ -49,6 +49,11 @@ public class XMLReader {
     }
     
     
+    /**
+     * This method loads a new file for parsing.
+     * @param file : a new xml file to read
+     * @throws XMLException : if the input file is not formatted properly
+     */
     public void loadNewFile(File file) throws XMLException {
     	try {
             DOCUMENT_BUILDER.reset();
@@ -63,6 +68,13 @@ public class XMLReader {
         }
     }
     
+    
+    /**
+     * This method tries to initialize a ConfigDoc with all of the initial parameters for the
+     * simulation. It determines which type of ConfigDoc to make from the XML file.
+     * @return ConfigDoc for the simulation
+     * @throws XMLException : if the ConfigDoc cannot be initialized for any reason
+     */
     public ConfigDoc getConfigDoc() throws XMLException {
     	//ugly if-statement to choose what kind of ConfigDoc to return
     	if(mySimType.equals(ConfigDoc.SIM_TYPE_PRED_PREY)){
@@ -76,25 +88,59 @@ public class XMLReader {
     	}
     }
     
+    
+    /**
+     * @return the simulation being run according to the XML file
+     */
     public String getSimType() {
     	return mySimType;
     }
-    public void setSimType(String type) {
-    	mySimType = type;
-    }
     
+   
+    /**
+     * This method looks through the XML file for the element specified by its tag. If successful,
+     * it returns the content as a String.
+     * @param tag : the xml tag to look for in the file
+     * @param index : specifies the i-th occurrence of this tag in the file
+     * @return the String content of the tag, if found
+     * @throws XMLException : if no tag match is found
+     */
     public String getString(String tag, int index) throws XMLException {
     	return getTextByTag(tag, index);
     }
     
+    /**
+     * This method looks through the XML file for the element specified by its tag. If successful,
+     * it returns the content casted to an Integer.
+     * @param tag : the xml tag to look for in the file
+     * @param index : specifies the i-th occurrence of this tag in the file
+     * @return the Integer content of the tag, if found
+     * @throws XMLException : if no tag match is found
+     */
     public int getInt(String tag, int index) throws XMLException {
     	return Integer.parseInt(getTextByTag(tag, index));
     }
     
+    /**
+     * This method looks through the XML file for the element specified by its tag. If successful,
+     * it returns the content as a Double.
+     * @param tag : the xml tag to look for in the file
+     * @param index : specifies the i-th occurrence of this tag in the file
+     * @return the Double content of the tag, if found
+     * @throws XMLException : if no tag match is found
+     */
     public double getDouble(String tag, int index) throws XMLException {
     	return Double.parseDouble(getTextByTag(tag, index));
     }
     
+    /**
+     * This method looks through the XML file for the element specified by its tag. If successful,
+     * it returns the content as a String.
+     * @param tag : the xml tag to look for in the file
+     * @param index : specifies the i-th occurrence of this tag in the file
+     * @return the String content of the tag, if found
+     * @throws XMLException : if no tag match is found
+     */
     private String getTextByTag(String tag, int index) throws XMLException {
     	NodeList nodeList = rootElement.getElementsByTagName(tag);
     	if (nodeList != null && nodeList.getLength() > index) {
@@ -103,6 +149,14 @@ public class XMLReader {
     	throw new XMLException(String.format(myResources.getString("ErrorTagNotFound"), tag));
     }
     
+    
+    /**
+     * This method takes several properly formatted strings, concatenates them, and writes them to a file.
+     * @param params : This is the current state of the simulation parameters, obtained from ConfigDoc
+     * @param neighbors : This is the list of neighbor offsets
+     * @param states : This is a complete specification of the current state of every cell in the grid.
+     * @throws XMLException : if it encounters any I/O errors
+     */
     public void writeToFile(String params, String neighbors, String states) throws XMLException {
     	Writer writer = null;
     	try  {
@@ -112,7 +166,7 @@ public class XMLReader {
     		throw new XMLException(e);
     	} finally {
     		if(writer != null) {
-    			try {writer.close();} catch(IOException e) {/*TODO:can ignore?*/}
+    			try {writer.close();} catch(IOException e) {/*can ignore?*/}
     		}
     	}
     }
